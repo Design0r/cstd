9b2026b3a35b4ffafe96e5585a7bc2e2f8bb7af3
alex <alex.mors@gmx.de>
2026-07-22 23:23:27 +0200

    feat: added more string functions

--- strings.h
+++ strings.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdbool.h>
+
 #include <string.h>
 
 typedef struct {
@@ -8,13 +8,16 @@ typedef struct {
 	const char *data;
 } String;
 
-typedef String Str;
+typedef struct {
+	size_t len;
+	const char *data;
+} Str;
 
 // ========
 // Utils
 // ========
-#define STR(s) (Str){.len = strlen(s), .data = s}
-#define STRING(s) (String){.len = strlen(s), .data = s}
+#define STR(s) (Str){.len = sizeof(s)-1, .data = s}
+#define STRING(s) (String){.len = sizeof(s)-1, .data = s}
 
 #define STR_FMT "%.*s"
 #define STR_ARG(s) s.len, s.data
@@ -36,9 +39,14 @@ static bool string_startswith_str(String s, Str slice);
 
 static Str string_to_str(String s);
 
+static void string_free(String s);
+
+
 // ========
 // Str
 // ========
+static Str str_from_cstr(const char *cstr);
+
 static Str str_trim_left(Str s);
 
 static Str str_trim_right(Str s);
@@ -52,6 +60,16 @@ static int str_split(Str s, char delim, Str *out, int out_size);
 static int str_split_str(Str s, Str delim, Str *out, int out_size);
 
 static bool str_eq(Str a, Str b);
+
+static bool str_startswith_str(Str a, Str b);
+
+static bool str_startswith_cstr(Str a, const char *cstr);
+
+static String str_to_string(Str s);
+
+static void str_to_cstr(Str s, char *buf);
+
+static char *str_to_cstr_m(Str s);
 
 // ========
 // Utils
@@ -68,7 +86,10 @@ static bool is_whitespace(const char c) {
 // String
 // ========
 static String string_from_cstr(const char *cstr) {
-	return (String){.len = strlen(cstr), .data = cstr};
+	size_t len = strlen(cstr);
+	char *new_data = (char *) malloc(len);
+	memcpy(new_data, cstr, len);
+	return (String){.len = len, .data = new_data};
 }
 
 static size_t string_len(const String s) {
@@ -77,11 +98,18 @@ static size_t string_len(const String s) {
 
 static bool string_startswith_str(const String s, const Str slice) {
 	if (s.len < slice.len) return false;
-	return memcmp(s.data, &slice.data, slice.len) == 0;
+	return memcmp(s.data, slice.data, slice.len) == 0;
 }
 
 static Str string_to_str(const String s) {
-	return (Str){.len = s.len, .data = s.data};
+	return (Str)
+	{
+		.len = s.len, .data = s.data
+	};
+}
+
+inline void string_free(String s) {
+	free((void *) s.data);
 }
 
 // ========
@@ -89,10 +117,9 @@ static Str string_to_str(const String s) {
 // ========
 static Str str_trim_left(Str s) {
 	for (size_t i = 0; i < s.len; i++) {
-		if (is_whitespace(s.data[i])) {
-			s.data++;
-			s.len--;
-		}
+		if (!is_whitespace(s.data[i])) break;
+		s.data++;
+		s.len--;
 	}
 
 	return s;
@@ -100,9 +127,8 @@ static Str str_trim_left(Str s) {
 
 static Str str_trim_right(Str s) {
 	for (size_t i = s.len - 1; i > 0; i--) {
-		if (is_whitespace(s.data[i])) {
-			s.len--;
-		}
+		if (!is_whitespace(s.data[i])) break;
+		s.len--;
 	}
 
 	return s;
@@ -128,18 +154,57 @@ static bool str_eq(const Str a, const Str b) {
 
 static int str_split(const Str s, const char delim, Str *out, int out_size) {
 	int found = 0;
-	size_t last_split = 0;
-	size_t len = 0;
+	size_t start = 0;
+
 	STR_FOR(s, i) {
-		char curr = s.data[i];
-		if (curr == delim || i == (s.len - 1)) {
-			out[found] = (Str){.len = len, .data = s.data + last_split + (found > 0 ? 1 : 0)};
-			found++;
-			last_split = i;
-			len = 0;
+		if (s.data[i] == delim) {
+			if (i > start) {
+				if (found >= out_size) return found;
+				out[found++] = (Str){.len = i - start, .data = s.data + start};
+			}
+			start = i + 1;
 		}
-		len++;
+	}
+
+	if (s.len > start && found < out_size) {
+		out[found++] = (Str){.len = s.len - start, .data = s.data + start};
 	}
 
 	return found;
+}
+
+static Str str_from_cstr(const char *cstr) {
+	return (Str)
+	{
+		.len = strlen(cstr), .data = cstr
+	};
+}
+
+static bool str_startswith_str(const Str a, const Str b) {
+	if (a.len < b.len) return false;
+	return memcmp(a.data, b.data, b.len) == 0;
+}
+
+static bool str_startswith_cstr(Str a, const char *cstr) {
+	size_t len = strlen(cstr);
+	if (a.len < len) return false;
+	return memcmp(a.data, cstr, len) == 0;
+}
+
+static String str_to_string(Str s) {
+	char *new_data = (char *) malloc(s.len);
+	memcpy(new_data, s.data, s.len);
+	return (String){.len = s.len, .data = new_data};
+}
+
+static char *str_to_cstr_m(Str s) {
+	char *new_data = (char *) malloc(s.len + 1);
+	memcpy(new_data, s.data, s.len);
+	new_data[s.len] = '\0';
+	return new_data;
+}
+
+static void str_to_cstr(Str s, char *buf) {
+	memcpy(buf, s.data, s.len);
+	buf[s.len] = '\0';
 }
